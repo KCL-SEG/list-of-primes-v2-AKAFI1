b1b148e41cb3ad11be154a9a4864d9535db94780
Kafi <ahmed.kafi@outlook.com>
2022-12-05 13:46:54 +0000

    code for primes v2

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    i=1
+    if number_of_primes <= 0:
+        raise ValueError('Number must be greater than 1')
+    if number_of_primes>=0:
+        while len(list)<number_of_primes:
+            factors = []
+            for n in range(1, i+1):
+                if i%n == 0: 
+                    factors.append(n)
+                elif len(factors) > 2:
+                    break
+
+            if len(factors) == 2:
+                list.append(i)
+            i += 1
+        return list
+    
